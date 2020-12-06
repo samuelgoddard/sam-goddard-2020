d21815973f161f1783b7f7debbee1dcd7e43b09f
Sam Goddard <hello@samgoddard.co.uk>
2020-12-06 17:56:57 +0000

    added rotating text in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+var ReactRotatingText = require('react-rotating-text');
 
 const Header = () => (
   <header className="container container--header fixed top-0 left-0 right-0 w-full z-30">
@@ -7,7 +8,16 @@ const Header = () => (
       <Link className="header-item hover:opacity-50 focus:opacity-50 " to="/"><span className="hidden md:block">Sam Goddard</span><span className="block md:hidden">SG</span></Link>
       <span className="header-item ml-3 md:ml-8 ">’89 —</span>
 
-      <span className="header-item ml-auto hidden lg:block ">Creative Developer</span>
+      <span className="header-item ml-auto hidden lg:block ">
+        <ReactRotatingText
+          items={["Creative Developer", "Sage One Trick", "Motion Developer", "Tailwind Advocate"]}
+          pause={4000}
+          emptyPause={250}
+          cursor={false}
+          typingInterval={40}
+          deletingInterval={40}
+        />
+      </span>
 
       <div className="ml-auto flex flex-wrap">
         <nav>
