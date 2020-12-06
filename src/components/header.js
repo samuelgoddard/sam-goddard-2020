5a407045be3113db9982419f052c4cbea83c49a9
Sam Goddard <hello@samgoddard.co.uk>
2020-12-06 17:48:52 +0000

    header sizing and padding added to projects for mobile vw

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header className="container container--header fixed top-0 left-0 right-0 w-full z-30">
     <div className="flex flex-wrap items-center border-b border-current border-opacity-75 pb-2">
       <Link className="header-item hover:opacity-50 focus:opacity-50 " to="/"><span className="hidden md:block">Sam Goddard</span><span className="block md:hidden">SG</span></Link>
-      <span className="header-item ml-4 md:ml-8 ">’89 —</span>
+      <span className="header-item ml-3 md:ml-8 ">’89 —</span>
 
       <span className="header-item ml-auto hidden lg:block ">Creative Developer</span>
 
@@ -17,7 +17,7 @@ const Header = () => (
           </ul>
         </nav>
 
-        <a href="mailto:hello@samgoddard.co.uk" className="header-item hover:opacity-50 focus:opacity-50  ml-8 md:ml-24"><span className="hidden md:inline">Let's </span>Collaborate</a>
+        <a href="mailto:hello@samgoddard.co.uk" className="header-item hover:opacity-50 focus:opacity-50 ml-5 md:ml-24"><span className="hidden md:inline">Let's </span>Collaborate</a>
       </div>
     </div>
   </header>
