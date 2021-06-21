97c1a6248be708d977f8b4c28bed66eedfe1a356
Sam Goddard <hello@samgoddard.co.uk>
2021-06-21 19:48:34 +0100

    updated availability

--- src/components/header.js
+++ src/components/header.js
@@ -14,59 +14,59 @@ const Header = ({ theme, path }) => {
             <Marquee velocity={0.04}>
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
               Creative Developer
               &nbsp;<em>—</em>&nbsp;
-              Available June ’21
+              Available Aug ’21
               &nbsp;<em>—</em>&nbsp;
             </Marquee>
           </div>
